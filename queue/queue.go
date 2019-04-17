6454208fe6f1c991f9435a660ac3e8269b78fb42
Ovidio R. Calvet <calveto@gmail.com>
2019-04-16 21:34:25 -0400

    Create Enqueue implementation

--- queue/queue.go
+++ queue/queue.go
@@ -8,7 +8,7 @@ type Queue struct {
 }
 
 type node struct {
-	prev *node
+	next *node
 	v    interface{}
 }
 
@@ -21,7 +21,13 @@ func New() *Queue {
 func (queue *Queue) Enqueue(v interface{}) {
 	if queue.root == nil {
 		queue.root = &node{nil, v}
+		return
 	}
+	var n *node
+	for n = queue.root; n.next != nil; n = n.next {
+	}
+	last := &node{nil, v}
+	n.next = last
 }
 
 // Dequeue removes an element from the queue
@@ -31,7 +37,7 @@ func (queue *Queue) Dequeue() interface{} {
 
 // Print prints the elements of the queue in the console
 func (queue *Queue) Print() {
-	for n := queue.root; n != nil; n = n.prev {
+	for n := queue.root; n != nil; n = n.next {
 		fmt.Println(n.v)
 	}
 }
