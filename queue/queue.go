bd5c9a5424d459f2341a711312d21063270666d7
Ovidio R. Calvet <calveto@gmail.com>
2019-04-16 21:26:07 -0400

    Add documentation to the queue

--- queue/queue.go
+++ queue/queue.go
@@ -12,20 +12,24 @@ type node struct {
 	v    interface{}
 }
 
+// New creates a new queue
 func New() *Queue {
 	return &Queue{}
 }
 
+// Enqueue queues an element into the queue
 func (Queue *Queue) Enqueue(v interface{}) {
 	if Queue.root == nil {
 		Queue.root = &node{nil, v}
 	}
 }
 
+// Dequeue removes an element from the queue
 func (Queue *Queue) Dequeue() interface{} {
 	return Queue.root.v
 }
 
+// Print prints the elements of the queue in the console
 func (Queue *Queue) Print() {
 	fmt.Println("Printing queue")
 }
