1d62215a221d392b2d93301ee154058cd7661cde
Ovidio R. Calvet <calveto@gmail.com>
2019-04-27 13:51:06 -0500

    Iterate from the sort function

--- bubble_sort/bubble_sort.go
+++ bubble_sort/bubble_sort.go
@@ -1,12 +1,20 @@
 package bubble_sort
 
 import (
+	"fmt"
 	"go_basics/linkedlist"
 )
 
 type Iter func() (interface{}, bool)
 
 func Sort(list *linkedlist.LinkedList) linkedlist.LinkedList {
-
+	// m := linkedlist.New()
+	iter := list.Iterator()
+	for v, hasNext := iter(); hasNext; {
+		if v != nil && hasNext {
+			fmt.Println(v)
+		}
+		v, hasNext = iter()
+	}
 	return *linkedlist.New()
 }
